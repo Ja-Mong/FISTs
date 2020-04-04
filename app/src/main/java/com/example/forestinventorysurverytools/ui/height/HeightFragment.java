445a772134debdc303a9bcd773473f4c506145d0
Ja-Mong <59469792+Ja-Mong@users.noreply.github.com>
2020-04-04 16:22:25 +0900

    get theta and height
    각도 및 수고 측정 공식 정리

--- app/src/main/java/com/example/forestinventorysurverytools/ui/height/HeightFragment.java
+++ app/src/main/java/com/example/forestinventorysurverytools/ui/height/HeightFragment.java
@@ -82,7 +82,7 @@ public class HeightFragment extends Fragment implements CameraAPI.Camera2Interfa
         mInputHeight = (EditText) root.findViewById(R.id.input_height);
 
         mBtn_height.setOnClickListener(measureHeight);
-        mBtn_calculate.setOnClickListener(calculateHeight);
+        mBtn_calculate.setOnClickListener(getCalculateHeight);
 
         return root;
     }
@@ -165,45 +165,23 @@ public class HeightFragment extends Fragment implements CameraAPI.Camera2Interfa
     }
 
     //n번 넣을 수 있게 하는 자료구조 전역 선언
-    Vector<Float> angle_vec = new Vector<Float>(); // 측정하는 모든 angle 값 저장
-    //Vector<double> dist_vec = new Vector<float>(); // 측정하는 구간들의 수고(높이)값 저장
+    Vector<Float> theta_vec = new Vector<Float>(); // 측정하는 모든 angle 값 저장
 
-    float distance = 0;
-    float T_height = 0;
-    float angleT = 0;
-    float angleXY = 0;
-    float angleX = 0;
-    float angleY = 0;
-    float angleNew = 0;
+    float f_theta = 0;
+    float t_theta = 0;
+    float xy_theta = 0;
+    float x_theta = 0;
+    float y_theta = 0;
+//    float y2_theta = 0;
+//    float n_theta = 0;
 
-    /**
-     * float f_theta = 0;
-     * float t_theta = 0;
-     * float xy_theta = 0;
-     * float x_theta = 0;
-     * float y_theta = 0;
-     * float n_theta = 0;
-     *
-     * final ImageButton.OnClickListener measureHeight = new ImageButton.OnClickListener() {
-     *
-     * if (click_count % 3 == 0) {
-     *     float phoneHeight = Float.volueOf(mInputHeight.getText().toString()) / 100f;
-     *     f_theta = Math.abs(mMySensorEventListener.getRoll());
-     *     click_count++;
-     * } else if (click_count % 3 == 1) {
-     *     t_theta = Math.abs(mMySensorEventListener.getRoll());
-     *     xy_theta = t_theta - f_theta;
-     *     x_theta = 90 - f_theta;
-     *     y_theta = Math.abs(xy_theta - x_theta);
-     *     click_count++;
-     * } else if (click_count % 3 == 2) {
-     *     t_theta = Math.abs(mMySensorEventListener.getRoll());
-     *     xy_theta = t_theta - f_theta;
-     *     y_theta = Math.abs(xy_theta - x_theta);
-     *     y_theta = y_theta - n_theta;
-     *     click_count++;
-     * }
-     */
+
+    // 이렇게 하게 되면 "theta_vec"에 저장되는 값은
+    // [0] 처음 측정했을 때의  f_theta
+    // [1] xy_theta - x_theta를 한 y_theta 값
+    // [2] y2_theta
+    // ....
+    // [N] angleYn == n번 측정했을 때의 y_theta (n_theta)
 
     //Button
     final ImageButton.OnClickListener measureHeight = new ImageButton.OnClickListener() {
@@ -212,96 +190,65 @@ public class HeightFragment extends Fragment implements CameraAPI.Camera2Interfa
 
             mMySensorEventListener.updateOrientationAngles();
             if (!mInputHeight.getText().toString().isEmpty()) {
-                if (click_count % 3 == 0) {
-                    float phoneHeight = Float.valueOf(mInputHeight.getText().toString()) / 100f;
-                    angle = Math.abs(mMySensorEventListener.getRoll());
-                    click_count++;
-                    showToast("1");
-                } else if (click_count % 3 == 1) {
-                    angleT = Math.abs(mMySensorEventListener.getRoll());
-                    angleXY = angleT - angle;
-                    angleX = 90 - angle;
-                    angleY = Math.abs(angleXY - angleX);
-                    click_count++;
-                    showToast("2");
-                }
+                if (theta_vec.isEmpty()) {
+                    f_theta = Math.abs(mMySensorEventListener.getRoll());
+                    theta_vec.add(f_theta);
+                    showToast(Integer.toString(theta_vec.size()));
+                } else if (theta_vec.size() == 1) {
+                    t_theta = Math.abs(mMySensorEventListener.getRoll());
+                    xy_theta = t_theta - theta_vec.elementAt(0);
+                    x_theta = 90 - theta_vec.elementAt(0);
+                    y_theta = Math.abs(xy_theta - x_theta);
+                    theta_vec.add(y_theta);
 
-
-                 /*--------------------------------------
-                 angle - 0번째 측정일때는 == f_theta 의미함
-                       - 1번째 측정일때는 == t_theta 의미함
-                 angleX ==  x_theta
-                 angleXY == xy_theta
-                 angleY == y_theta
-
-                //(방법1*) 위 각도 계산을 "calculate" 에서 진행할 경우
-                //1. angle 값을 받아와 임시 변수에 할당
-                //2. 자료구조에 angle 값  저장
-                //3. 몇번 측정했는지 알림( 기능 구현 후에는 지워도 됨 )
-
-                //angle = Math.abs(mMySensorEventListener.getRoll()); //1
-                //angle_vec.add(angle); //2
-                //showToast(Integer.toString(angle_vec.size())); //3
-
-                (방법2*) 각도 계산을 하고 값을 저장하려 할 경우, 즉 "cal"에서는 거리만 계산할 경우
-
-                // 이렇게 하게 되면 "angle_vec"에 저장되는 값은
-                // [0] 처음 측정했을 때의 angle 값 == f_theta
-                // [1] angleXY-angleX를 한 angleY 값 == y_theta
-                // [2] angleY2  == y_theta
-                // ....
-                // [N] angleYn == n번 측정했을 때의 y_theta (n_theta)
-
-                //if(angle_vec.isEmpty()){
-                // 처음 벡터가 비어있을 때는 위와 같이 진행
-                    angle = Math.abs(mMySensorEventListener.getRoll());
-                    angle_vec.add(angle); //2
-                    showToast(Integer.toString(angle_vec.size())); //3
-                //}else if(angle_vec.size()==1){
-                //2번이거나, N번의 중간지점일 경우
-                // angleX는 고정된 값이므로 여기서 변수에 할당 및 저장
-                //("angle_vec"에는 저장 하지 않음)
-                //    angle = Math.abs(mMySensorEventListener.getRoll());
-                //    angleXY = angle - angle_vec.elementAt(0);
-                //    angleX = 90 - angle_vec.elementAt(0);
-                //    angleY = Math.abs(angleXY - angleX);
-                //    angle_vec.add(angleY);
-                //}else
-                //{
-                    angle = Math.abs(mMySensorEventListener.getRoll());
-                    angleXY = angle - angle_vec.elementAt(0);
-                    angleY = Math.abs(angleXY - angleX);
-                    y_theta = y_theta - n_theta; // n_theta ??
-                    angle_vec.add(angleY);
-                //}
-                //>> 저장하는 방식에 따라 계산식에서 자료 다루는 방법이 조금씩 다름
-                -----------------------------------------*/
-
-
+                    /**
+                     * else 부분이 어떤걸 뜻하는 건지 잘 모르겠음
+                     **/
+//                } else {
+//                    t_theta = Math.abs(mMySensorEventListener.getRoll());
+//                    xy_theta = t_theta - theta_vec.elementAt(0);
+//                    y_theta = Math.abs(xy_theta - x_theta);
+//                    y2_theta = y_theta - n_theta; // n_theta ??
+//                    theta_vec.add(y_theta);
 
                 }
             }
-        };
+        }
+    };
 
-    /**
-     * double distance = 0;
-     * double t_height = 0;
-     * double x_height = 0;
-     * double x1_height = 0;
-     *
-     * final ImageButton.OnClickListener calculateHeight = new ImageButton.OnClickListener() {
-     *
-     * if (calculate.getId() == R.id.Btn_calculate) {
-     *     float phoneHeight = Float.valueOf(mInputHeight.getText().toString()) / 100f;
-     *     distance = phoneHeight * Math.tan(y_theta); //distance: 지속적으로 갱신되는 값 / y_theta: 지속적으로 갱신되는 값
-     *     x_height = distance * Math.tan(y_theta);
-     *     x1_height = distance * Math.tan(y_theta);
-     *     n_height = x1_height - x_height;
-     *     t_height = x_height + n_height + phoneHeight;
-     * }
-     *
-     */
-                                /* angel_vec : 구간별 angel 벡터, dist_vec : 구간별 수고 벡터*/
+    /* theta_vec : 구간별 theta 벡터, dist_vec : 구간별 수고 벡터*/
+
+    Vector<Double> height_vec = new Vector<Double>(); // 측정하는 모든 angle 값 저장
+
+    double x_height;
+    double t_height;
+    double new_height;
+
+    float phoneHeight = Float.valueOf(mInputHeight.getText().toString()) /100f;
+    float distance = (float) (Math.tan(x_theta) * phoneHeight);
+
+    final ImageButton.OnClickListener getCalculateHeight = new ImageButton.OnClickListener() {
+        @Override
+        public void onClick(View calculate) {
+            if (calculate.getId() == R.id.Btn_calculate) {
+                for (int i =0; i<theta_vec.size(); i++) {
+                    if (height_vec.isEmpty()) {
+                        x_height = distance * Math.tan(theta_vec.elementAt(i+1));
+                        height_vec.add(x_height);
+                        t_height += x_height;
+                    } else {
+                        t_height = distance * Math.tan(theta_vec.elementAt(i+1));
+                        new_height = t_height - height_vec.elementAt(i);
+                        height_vec.add(new_height);
+                        t_height += new_height;
+                    }
+                }
+                t_height += phoneHeight;
+            }
+        }
+    };
+
+
     /*
      * (방법 1일 경우)  여기서 각도 계산하면서 수고까지 함께 계산 후 총 수고 계산
      *
@@ -337,47 +284,18 @@ public class HeightFragment extends Fragment implements CameraAPI.Camera2Interfa
     * 리셋 버튼에 적용할 예정이고 테스트하기 위해 추가했던 부분이므로 제거하였습니다.
     * */
 
-        double length = 0.0;
-        double angleCalc = 0.0;
-        double dist = 0.0;
-        double finalDisp = 0.0;
-        double height = 0.0;
-        double angleCal = 0.0;
-
-        ImageButton.OnClickListener calculateHeight = new ImageButton.OnClickListener() {
-            @Override
-            public void onClick(View calculate) {
-                if (calculate.getId() == R.id.Btn_calculate) {
-                    float phoneHeight = Float.valueOf(mInputHeight.getText().toString()) / 100f;
-                    double distance = phoneHeight * Math.tan(angleY); //distance를 구하는  angle 값이 계속 새로운 angle이 들어와야 될 수 도 있음.. Test 필요
-                    double T_height = distance * Math.tan(angleY);
-//                double T_height2 = distance * Math.tan(angleNew);
-//                double New_height = T_height2 - T_height;
-                    double height = T_height + phoneHeight;
-
-                    String height_value = String.format("%.1f", height);
-                    mHeight_tv.setText("수        고 :" + height_value + "m");
-                    showToast("calculate");
-
-                }// end if
-
-
-            }
-        };
-
-
-        //Handler
-        private void startCameraHandlerThread() {
-            mCameraThread = new HandlerThread("Camera Background");
-            mCameraThread.start();
-            mCameraHandler = new Handler(mCameraThread.getLooper());
-        }
-
-        private void stopCameraHandlerThread() {
-            mCameraThread.quitSafely();
-            mCameraThread = null;
-            mCameraHandler = null;
-        }
+    //Handler
+    private void startCameraHandlerThread() {
+        mCameraThread = new HandlerThread("Camera Background");
+        mCameraThread.start();
+        mCameraHandler = new Handler(mCameraThread.getLooper());
     }
+
+    private void stopCameraHandlerThread() {
+        mCameraThread.quitSafely();
+        mCameraThread = null;
+        mCameraHandler = null;
+    }
+}
 
 
